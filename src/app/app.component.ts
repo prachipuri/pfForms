93a943d0f11e83572bcf4f68f3ad0a595aa84229
Prachi Puri <80587997+prachipuri@users.noreply.github.com>
2022-07-08 20:06:07 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pfForms';
+  headers = ["DOCUMENT NAME", "DOCUMENT LINK"]
+}
